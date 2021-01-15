c14038a2d7639e2ce3ebb609ede67ed9e354209f
lagosyusuf <ubuntu@ip-172-31-23-87.us-east-2.compute.internal>
2021-01-15 23:24:21 +0000

    added a new print statement

--- hello.java
+++ hello.java
@@ -1,6 +1,8 @@
 // This java programm will print a sample message
 class Java{  
-    public static void main(String args[]){  
-    System.out.println("Hello Java");  
+    public static void main(String args[]) {  
+    System.out.println("Hello Java");
+    
+    System.out.println("Welcome to yusuf world");
  }  
 }  
