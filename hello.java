98f02d5f478729eec45e33e2cd55dda62d04e9b4
root <root@ip-172-31-23-87.us-east-2.compute.internal>
2021-01-16 15:05:15 +0000

    Added code release to feature1

--- hello.java
+++ hello.java
@@ -1,5 +1,7 @@
 class Java{  
     public static void main(String args[]){  
-    System.out.println("Hello Java");  
+    System.out.println("Hello Java");
+
+    System.out.println("Hello Branching")  
  }  
 }  
