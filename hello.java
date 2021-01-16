b4ebae9a485e976510de45b58700036fe5593702
lagosyusuf <ubuntu@ip-172-31-23-87.us-east-2.compute.internal>
2021-01-16 04:00:32 +0000

    Restore hello.javafile from commit:c14038a

--- hello.java
+++ hello.java
@@ -1,8 +1,5 @@
-// This java programm will print a sample message
 class Java{  
-    public static void main(String args[]) {  
-    System.out.println("Hello Java");
-    
-    System.out.println("Welcome to yusuf world");
+    public static void main(String args[]){  
+    System.out.println("Hello Java");  
  }  
 }  
